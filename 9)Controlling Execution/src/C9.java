b8f3285f3ccf999889a2d60fc2c248af792300e3
gromov-nikita <hromay.n999@gmail.com>
2021-02-23 21:17:06 +0300

    1

--- 9)Controlling Execution/src/C9.java
+++ 9)Controlling Execution/src/C9.java
@@ -7,22 +7,22 @@ name of the class) the output will be: 1, 1, 2, 3, 5.
  */
 class C9 {
     //1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181
-    void FibonacciPrint(int ans) {
+    void fibonacciPrint(int ans) {
         System.out.print(ans + " ");
     }
-    int Fibonacci(int n, int pred, int ans) {
+    int fibonacci(int n, int pred, int ans) {
+        fibonacciPrint(ans);
         if((ans + pred) <= n){
             int save = ans;
             ans += pred;
             pred = save;
-            FibonacciPrint(Fibonacci(n,pred,ans));
+            fibonacci(n,pred,ans);
         }
         return ans;
     }
     public static void main(String[] args) {
         C9 obj = new C9();
-        obj.FibonacciPrint(obj.Fibonacci(10000,0,1));
-        System.out.println(1);
+        obj.fibonacci(10000,0,1);
     }
 
 }
